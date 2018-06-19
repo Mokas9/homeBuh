fac7c464dcd41ba198d118c4de550961980d98a1
Maxim Grushevsky <m.grushevsky10@gmail.com>
2018-06-19 17:35:29 +0300

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
